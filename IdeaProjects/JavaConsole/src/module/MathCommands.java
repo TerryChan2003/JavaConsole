b369e1f0171e7fcdbcde925d18a67a09fbf76143
deax <aleksandr_h2001@mail.ru>
2017-09-18 19:59:09 +0300

    Update in class GameComx

--- IdeaProjects/JavaConsole/src/module/MathCommands.java
+++ IdeaProjects/JavaConsole/src/module/MathCommands.java
@@ -12,69 +12,68 @@ class MathCommands {
             'u', 'v', 'w', 'x', 'y', 'z'
     };
 
-    static String toBiter(int in, int to, String byt, boolean com) throws Exception {
+    private static BigInteger toTen(boolean com, int in, String byt) {
+        int step;
+        BigInteger j;
+        BigInteger byter = BigInteger.valueOf(0);
+        if (com) System.out.println("Форматировка числа:");
+        for (int i = 0; i < byt.length(); i++) {
+            for (int b = 0; b < in; b++) {
+                if (byt.charAt(i) != '0' && byt.charAt(i) != '-') {
+                    if (byt.charAt(i) == digits[b]) {
+                        step = (byt.length() - 1 - i);
+                        j = BigInteger.valueOf(in).pow(step);
+                        j = j.multiply(BigInteger.valueOf(b));
+                        if (com) {
+                            if (i == 0) {
+                                System.out.print(in + "^" + step + "*" + b + "(" + j + ")");
+                            } else {
+                                System.out.print(" + " + in + "^" + step + "*" + b + "(" + j + ")");
+                            }
+                        }
+                        byter = byter.add(j);
+                        break;
+                    }
+                }
+            }
+        }
+        if (com) System.out.println(" = " + byter);
+        return byter;
+    }
+
+    static StringBuilder ToHexer(boolean com, BigInteger byter, StringBuilder str, int to, String byt) {
         BigInteger[] jk;
+        short ost;
+        if (byter.toString().equals("0")) byter = new BigInteger(byt).abs();
+        while (!byter.toString().equals("0")) {
+            jk = byter.divideAndRemainder(BigInteger.valueOf(to));
+            ost = Short.valueOf(String.valueOf(jk[1]));
+            str.insert(0, digits[ost]);
+            if (com)
+                System.out.println("Вычисляем остаток: " + byter + " / " + to + " = " + jk[0] + " (остаток " + ost + ", в системе " + digits[ost] + ").");
+            byter = jk[0];
+        }
+        return str;
+    }
+
+    static String toBiter(int in, int to, String byt, boolean com) throws Exception {
         StringBuilder str = new StringBuilder("");
         BigInteger byter;
         byter = BigInteger.valueOf(0);
-        BigInteger j;
-        int step;
         boolean isTenIn = in == 10;
         boolean isTenTo = to == 10;
-        short ost;
-        if (to >= 2 && to <= 36) {
-            if (in >= 2 && in <= 36) {
-                if (!isTenIn) {
-                    if (com) {
-                        System.out.println("Форматировка числа:");
-                    }
-                    for (int i = 0; i < byt.length(); i++) {
-                        for (int b = 0; b < in; b++) {
-                            if (byt.charAt(i) != '0' && byt.charAt(i) != '-') {
-                                if (byt.charAt(i) == digits[b]) {
-                                    step = (byt.length() - 1 - i);
-                                    j = BigInteger.valueOf(in).pow(step);
-                                    j = j.multiply(BigInteger.valueOf(b));
-                                    if (com) {
-                                        if (i == 0) {
-                                            System.out.print(in + "^" + step + "*" + b + "(" + j + ")");
-                                        } else {
-                                            System.out.print(" + " + in + "^" + step + "*" + b + "(" + j + ")");
-                                        }
-                                    }
-                                    byter = byter.add(j);
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    System.out.println(" = " + byter);
-                }
-                if (!isTenTo) {
-                    if (byter.toString().equals("0")) {
-                        byter = new BigInteger(byt).abs();
-                    }
-                    while (!byter.toString().equals("0")) {
-                        jk = byter.divideAndRemainder(BigInteger.valueOf(to));
-                        ost = Short.valueOf(String.valueOf(jk[1]));
-                        str.insert(0, digits[ost]);
-                        if (com) {
-                            System.out.println("Вычисляем остаток: " + byter + " / " + to + " = " + jk[0] + " (остаток " + ost + ", в системе " + digits[ost] + ").");
-                        }
-                        byter = jk[0];
-                    }
-                }
-                if (isTenTo) {
-                    str.insert(0, "Получаем: " + byter);
-                }
-                if (com && !isTenTo) {
-                    str.insert(0, "Соединяем остатки снизу вверх, получаем: ");
-                }
+        if (to >= 2 && to <= digits.length) {
+            if (in >= 2 && in <= digits.length) {
+                if (!isTenIn) byter = toTen(com, in, byt);
+                if (!isTenTo) str = ToHexer(com, byter, str, to, byt);
+                if (isTenTo && com) str.insert(0, "Получаем: " + byter);
+                if (com && !isTenTo) str.insert(0, "Соединяем остатки снизу вверх, получаем: ");
                 return str.toString();
             } else System.out.println("Неверный ввод первого параметра");
         } else System.out.println("Неверный ввод второго параметра");
         return str.toString();
     }
+
 
     static BigInteger factorial(int g) {
         return g > 1 ? factorial(g - 1).multiply(BigInteger.valueOf(g)) : BigInteger.valueOf(1);
